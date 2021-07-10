14421e080000f10c2ed49b0c8b61321db1fae2d5
Yusuf Mohamed <yusuf_mohamed@cysiv.com>
2021-07-10 14:35:54 +0200

    compatibility with ida 7.4 - up
    updated getinputmd5

--- FinSpyDeob.py
+++ FinSpyDeob.py
@@ -42,7 +42,8 @@ class deobx86_t(idaapi.plugin_t):
 
 	def init(self):
 		self.hook = None
-		if idc.GetInputMD5() != input_md5 or idaapi.ph_get_id() != idaapi.PLFM_386:
+		file_md5 = ''.join(['{0:02x}'.format(i).upper() for i in idc.retrieve_input_file_md5()])
+		if file_md5 != input_md5 or idaapi.ph_get_id() != idaapi.PLFM_386:
 			return idaapi.PLUGIN_SKIP
 				
 		self.hook = deobX86Hook()
